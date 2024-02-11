fe5b82e4b2dd3ce077f00cfc76889ac7dd88ec46
Genner GR <lopezitzepcgenneralexander@gmail.com>
2024-02-10 19:30:53 -0600

    'instalanding development environment' 'Variables && constantes'

---
+++ Scripts/variables_constantes.js
@@ -0,0 +1,21 @@
+let a =  'hola';
+let num1 = 1;
+let num2 = 2;
+
+num2 = 5;
+
+let resultado = num1 + num2;
+
+console.log(a + ' la suma de ' + num1 + ' + ' + num2 + ' es igual a ' + resultado);
+
+const PI = 3.1416;
+
+let objeto = {
+    marca: 'honda',
+    modelo: 'cb650r'
+}
+
+let lenguajes = ['Js', 'Python', 'Php']
+
+console.log()
+
